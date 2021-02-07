85ceb1276d57fdf12dfe498a781ed089025e48d3
sick <andie_wand@gmx.de>
2021-02-07 20:56:33 +0100

    prevent reminder loop on error in cron

--- src/cron/ReminderCron.ts
+++ src/cron/ReminderCron.ts
@@ -1,6 +1,5 @@
 import * as cron from 'node-cron';
 import {ReminderService} from "../service/ReminderService";
-import {Reminder} from "../model/Reminder";
 import {currentTimestamp, stringFormatUnixTimestamp} from "../model/Time";
 import axios from 'axios';
 
@@ -17,42 +16,45 @@ export default function schedule(){
 
         for (const reminder of entries) {
             reminder.remindedAtUnix = currentTimestamp();
+            await reminder.save();    //save remindedAt before reminding prevents reminder-loops (potential spam) on failure
 
-            if(reminder.isRecurring){
-                const nextSchedule = reminder.reschedule();
-                nextSchedule.save().then(res=>{
-                    console.log(
-                        `${reminder.prevRecurringId} -> ${reminder.id} -> ${nextSchedule.id}`,
-                        'rescheduled from ', stringFormatUnixTimestamp(reminder.remindAtUnix), 'to', stringFormatUnixTimestamp(nextSchedule.remindAtUnix));
+            try {
+                if (reminder.isRecurring) {
+                    const nextSchedule = reminder.reschedule();
+                    nextSchedule.save().then(res => {
+                        console.log(
+                            `${reminder.prevRecurringId} -> ${reminder.id} -> ${nextSchedule.id}`,
+                            'rescheduled from ', stringFormatUnixTimestamp(reminder.remindAtUnix), 'to', stringFormatUnixTimestamp(nextSchedule.remindAtUnix));
 
-                });
-            }
-
-            console.log('query user data for reminder', reminder);
-            const userData = await axios.get(process.env.DATABASE_USER_ENDPOINT + reminder.userId);
-            const userClients = (userData && userData.data ? userData.data.messengerIDs : []).map(messengerData => ({
-                serviceName: messengerData.messenger,
-                clientId: messengerData.id
-            }))
-            console.log('user clients', userClients)
-
-            for (const {serviceName, clientId} of userClients) {
-                try{
-                    const data = {
-                        token: process.env.USERMESSENGER_TOKEN,
-                        service: serviceName,
-                        user: clientId,
-                        message: 'Erinnerung: ' + reminder.title
-                    };
-                    console.log('posting to usermessenger', data)
-                    const response = axios.post(process.env.USERMESSENGER_ENDPOINT, data);
+                    });
                 }
-                catch (e) {
-                    console.log('could not send to usermessenger', e);
+
+                console.log('query user data for reminder', reminder);
+                const userData = await axios.get(process.env.DATABASE_USER_ENDPOINT + reminder.userId);
+                const userClients = (userData && userData.data ? userData.data.messengerIDs : []).map(messengerData => ({
+                    serviceName: messengerData.messenger,
+                    clientId: messengerData.id
+                }))
+                console.log('user clients', userClients)
+
+                for (const {serviceName, clientId} of userClients) {
+                    try {
+                        const data = {
+                            token: process.env.USERMESSENGER_TOKEN,
+                            service: serviceName,
+                            user: clientId,
+                            message: 'Erinnerung: ' + reminder.title
+                        };
+                        console.log('posting to usermessenger', data)
+                        const response = axios.post(process.env.USERMESSENGER_ENDPOINT, data);
+                    } catch (e) {
+                        console.log('could not send to usermessenger', e);
+                    }
                 }
             }
-
-            reminder.save();
+            catch (e) {
+                console.error('Could not process reminder', e)
+            }
         }
 
 
